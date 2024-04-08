e7d9d96bb4eb8e28805722a13db680ad02c6b1bf
HuyNguyen <huy6495@gmail.com>
2024-04-08 14:50:56 +0700

    set up toolbar

--- app/src/main/java/com/example/animalList/MainActivity.java
+++ app/src/main/java/com/example/animalList/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.animalList;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.animalList.adapter.ExamAdapter;
@@ -24,17 +28,20 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar= (Toolbar)findViewById(R.id.toolbar);
-        toolbar.setTitle("");
-        setSupportActionBar(toolbar);
 
-        List<Exam> list = new ArrayList<>();
-        list = getData();
+        DrawerLayout drawerLayout = findViewById(R.id.my_drawer_layout);
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
 
-        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
-        adapter = new ExamAdapter(list, getApplication());
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        // pass the Open and Close toggle for the drawer layout listener
+        // to toggle the button
+        drawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+        // to make the Navigation drawer icon always appear on the action bar
+        if (getSupportActionBar() != null) {
+            // Enable the "up" button (the hamburger icon)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
     }
 
 
