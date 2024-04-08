cc049a67273c5420ad52a7f12233b5f5f74af187
HuyNguyen <huy6495@gmail.com>
2024-04-08 16:29:04 +0700

    re-test

--- app/src/main/java/com/example/animalList/MainActivity.java
+++ app/src/main/java/com/example/animalList/MainActivity.java
@@ -1,66 +1,35 @@
 package com.example.animalList;
 
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Bundle;
-import android.view.View;
-
-import androidx.appcompat.widget.Toolbar;
-
-import com.example.animalList.adapter.ExamAdapter;
-import com.example.animalList.model.Exam;
-
-import java.util.List;
-import java.util.ArrayList;
-
-
-public class MainActivity extends AppCompatActivity
-{
-    ExamAdapter adapter;
-    RecyclerView recyclerView;
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DrawerLayout drawerLayout = findViewById(R.id.my_drawer_layout);
-        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
-
-        // pass the Open and Close toggle for the drawer layout listener
-        // to toggle the button
+        drawerLayout = findViewById(R.id.my_drawer_layout);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.nav_open, R.string.nav_close);
         drawerLayout.addDrawerListener(actionBarDrawerToggle);
         actionBarDrawerToggle.syncState();
 
-        // to make the Navigation drawer icon always appear on the action bar
-        if (getSupportActionBar() != null) {
-            // Enable the "up" button (the hamburger icon)
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
         }
+        return super.onOptionsItemSelected(item);
     }
-
-
-    // Sample data for RecyclerView
-    private List<Exam> getData()
-    {
-        List<Exam> list = new ArrayList<>();
-        list.add(new Exam("First Exam",
-                "May 23, 2015",
-                "Best Of Luck"));
-        list.add(new Exam("Second Exam",
-                "June 09, 2015",
-                "b of l"));
-        list.add(new Exam("My Test Exam",
-                "April 27, 2017",
-                "This is testing exam .."));
-
-        return list;
-    }
-
-
 }
