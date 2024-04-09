1b5ece9507a8bf9f771763805c2ab68b8634a270
HuyNguyen <huy6495@gmail.com>
2024-04-09 17:45:09 +0700

    feat: add recycle item layout

---
+++ app/src/main/java/com/example/animalList/model/KindOfAnimal.java
@@ -0,0 +1,22 @@
+package com.example.animalList.model;
+
+import android.graphics.Bitmap;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KindOfAnimal {
+
+    public String name;
+    public int photo;
+    public ArrayList<Animal> listRepresent;
+
+    public KindOfAnimal(String name, int photo,ArrayList<Animal> listRepresent)
+        {
+            this.name = name;
+            this.photo = photo;
+            this.listRepresent = listRepresent;
+        }
+
+
+}
---
+++ app/src/main/java/com/example/animalList/model/KindOfAnimalViewHolder.java
@@ -0,0 +1,19 @@
+package com.example.animalList.model;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.animalList.R;
+
+public class KindOfAnimalViewHolder extends RecyclerView.ViewHolder {
+    private TextView kindName;
+    private ImageView kindPhoto;
+
+    public  KindOfAnimalViewHolder(View itemView){
+        super(itemView);
+//        kindName = itemView.findViewById(R.id.)
+    }
+}
