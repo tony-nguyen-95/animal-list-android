fdf467da7500c24aad11e4c9a695766277cb6836
HuyNguyen <huy6495@gmail.com>
2024-04-28 16:34:45 +0700

    lab3.6

--- app/src/main/java/com/example/animalList/MainActivity.java
+++ app/src/main/java/com/example/animalList/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.animalList;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -10,6 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // Inflate the loading layout
+        View loadingView = getLayoutInflater().inflate(R.layout.loading_layout, null);
+
+        // Add the loading layout to the main layout of the activity
+        ViewGroup rootView = findViewById(android.R.id.content);
+        rootView.addView(loadingView);
     }
 
 }
