d4154548eb61db3abffff19e69363397bc8bbded
HuyNguyen <huy6495@gmail.com>
2024-04-28 16:23:17 +0700

    remove unused package

--- app/src/main/java/com/example/animalList/MainActivity.java
+++ app/src/main/java/com/example/animalList/MainActivity.java
@@ -2,14 +2,7 @@ package com.example.animalList;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.GridView;
-
-import com.example.animalList.model.Animal;
-import com.example.animalList.adapter.AnimalAdapter;
-import java.util.List;
-import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
