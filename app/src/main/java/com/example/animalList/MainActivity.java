1162f73f099dbd0e5b2de408d89d57b10e5503ca
HuyNguyen <huy6495@gmail.com>
2024-05-01 17:20:12 +0700

    lab4.2

--- app/src/main/java/com/example/animalList/MainActivity.java
+++ app/src/main/java/com/example/animalList/MainActivity.java
@@ -9,12 +9,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Instantiate FragmentFaceEmoji
-        FragmentFaceEmoji fragmentFaceEmoji = new FragmentFaceEmoji();
-
-        // Begin a transaction to replace the fragment container with FragmentFaceEmoji
-        getSupportFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, fragmentFaceEmoji)
-                .commit();
     }
 }
