50b7558f824b4f6f85fdf3b732ba2a7fbfe2bce8
HuyNguyen <huy6495@gmail.com>
2024-05-01 16:16:27 +0700

    lab4.1

--- app/src/main/java/com/example/animalList/MainActivity.java
+++ app/src/main/java/com/example/animalList/MainActivity.java
@@ -1,34 +1,20 @@
 package com.example.animalList;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.Toast;
-
-
 
 public class MainActivity extends AppCompatActivity {
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
-        setContentView(R.layout.button_toast);
+        // Instantiate FragmentFaceEmoji
+        FragmentFaceEmoji fragmentFaceEmoji = new FragmentFaceEmoji();
 
-        Button clickMe = findViewById(R.id.click_me);
-        Button emptyButton = findViewById(R.id.empty_button);
-
-        clickMe.setOnClickListener(v -> {
-            Toast.makeText(this, "Clicked me", Toast.LENGTH_SHORT).show();
-        });
-
-        emptyButton.setOnClickListener(v->{
-            Toast.makeText(this, "Empty button", Toast.LENGTH_SHORT).show();
-        });
-
+        // Begin a transaction to replace the fragment container with FragmentFaceEmoji
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragmentFaceEmoji)
+                .commit();
     }
-
 }
