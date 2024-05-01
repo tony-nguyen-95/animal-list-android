7317c4bb99a80816f1cd6f75bb631cfcc015f553
HuyNguyen <huy6495@gmail.com>
2024-05-01 17:24:53 +0700

    lab4.3

--- app/src/main/java/com/example/animalList/MainActivity.java
+++ app/src/main/java/com/example/animalList/MainActivity.java
@@ -1,13 +1,72 @@
 package com.example.animalList;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
 
+    private void initView() {
+        findViewById(R.id.fr_mom).setOnClickListener(this);
+        findViewById(R.id.fr_dad).setOnClickListener(this);
+        findViewById(R.id.fr_crush).setOnClickListener(this);
+        findViewById(R.id.fr_best_friend).setOnClickListener(this);
+        findViewById(R.id.iv_dialer).setOnClickListener(this);
+    }
+
+    @Override
+
+    public void onClick(View v) {
+        v.startAnimation(AnimationUtils.loadAnimation(this, androidx.appcompat.R.anim.abc_popup_enter));
+        if (v instanceof FrameLayout) {
+            processCall((String) v.getTag());
+            return;
+        }
+        //Hiển thị dialog thông báo
+        AlertDialog alert = new AlertDialog.Builder(this).create();
+        alert.setTitle("Thông báo");
+        alert.setMessage("Mở màn hình quay số?");
+        alert.setButton(AlertDialog.BUTTON_POSITIVE, "OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                gotoDialPad();
+            }
+        });
+        gotoDialPad();
+    }
+
+    private void gotoDialPad() {
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        startActivity(intent);
+    }
+
+
+    private void processCall(String phone) {
+        if (checkSelfPermission(Manifest.permission.CALL_PHONE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CALL_PHONE}, 101);
+            Toast.makeText(this, "Hãy thực hiện lại sau khi cấp quyền!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse("tel: " + phone));
+        startActivity(intent);
     }
 }
